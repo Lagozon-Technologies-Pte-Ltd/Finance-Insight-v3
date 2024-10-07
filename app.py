a6964751ff797439f18b1dc24c3cc98fcd39efb1
rohitvermaAI <168617578+rohitvermaAI@users.noreply.github.com>
2024-10-07 18:18:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,3 +83,6 @@ def upload_file(): #file uploadation
 
     return render_template('results.html', results=results)
 
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
